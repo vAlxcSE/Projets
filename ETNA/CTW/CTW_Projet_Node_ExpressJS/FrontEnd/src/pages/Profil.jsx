a9d765ed950acc1a728191408674dc952a55b7ab
vAlxcSE <alex_yh@outlook.fr>
2024-06-10 14:09:41 +0200

    MAJ

--- ETNA/CTW/CTW_Projet_Node_ExpressJS/FrontEnd/src/pages/Profil.jsx
+++ ETNA/CTW/CTW_Projet_Node_ExpressJS/FrontEnd/src/pages/Profil.jsx
@@ -25,7 +25,7 @@ export default function Profil() {
               <div className="mt-2">
                 <input
                   type="text"
-    
+                  placeholder="First Name"
                   id="first-name"
                   
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
